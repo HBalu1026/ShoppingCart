c72152167c8da5453106f31a0d86a6a905633dfe
hernadi.balazs <hernadi.balazs@BURG-1E06-20>
2025-01-24 09:52:10 +0100

    kézs

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -13,5 +13,44 @@
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
 
+        public void AddProduct(string name, double price)
+        {
+            _products.Add(new Product(name, price));
+        }
+
+        public void RemoveProduct(string name)
+        {
+            Product? product = _products.Find(x => x.Name == name);
+            if (product == null)
+            { throw new InvalidOperationException(); }
+            else
+            { _products.Remove(product); }
+        }
+
+        public double GetTotalPrice()
+        {
+            double total = 0;
+            foreach (var product in _products)
+            {
+                total += product.Price;
+            }
+            return total;
+        }
+
+        public List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+            try
+            {
+                products.AddRange(_products);
+            }
+            catch
+            {
+                throw new Exception();
+            }
+
+            return products;
+
+        }
     }
 }
